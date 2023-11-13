abffa3d23a73639bf7bf2d4756e2204b01879661
Éric Alonso Martínez <113835039+eralonso@users.noreply.github.com>
2023-11-13 13:20:08 +0100

    WebServ < parser > - [ Parsing each option inside a server block ]

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -6,7 +6,7 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 12:48:38 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/11/07 15:13:03 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/11/13 12:52:10 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,71 +17,75 @@
 #include <fstream>
 #include <TreeSplit.hpp>
 
-std::vector<std::string>& ConfigParser::split(std::vector<std::string>& v,
+std::vector<std::string>& ConfigParser::split( std::vector<std::string>& v,
 	std::string strArr,
-	std::string delimiter)
+	std::string delimiter )
 {
 	std::string	temp;
     int starting = 0;
-    int ending = strArr.find(delimiter);
-    while (ending!=-1) {
-		temp = strArr.substr(starting, ending-starting);
-		if (temp.length()> 0)
-        	v.push_back(temp);
+    int ending = strArr.find( delimiter );
+    while ( ending != -1 )
+	{
+		temp = strArr.substr( starting, ending-starting );
+		if ( temp.length() > 0 )
+        	v.push_back( temp );
         starting = ending + delimiter.size();
-        ending = strArr.find(delimiter, starting);
+        ending = strArr.find( delimiter, starting );
     }
-	temp = strArr.substr(starting, ending-starting);
-	if (temp.length()> 0)
-		v.push_back(temp);
-	return (v);
+	temp = strArr.substr( starting, ending-starting );
+	if ( temp.length() > 0 )
+		v.push_back( temp );
+	return ( v );
 }
 
-ConfigParser::ConfigParser(int argc, char **argv)
+ConfigParser::ConfigParser( int argc, char **argv )
 {
 	std::ifstream	myfile;
 	std::string		line;
 	std::string		content;
+	std::string		head;
+	std::string		body;
 
-	if (argc != 2)
+	if ( argc != 2 )
 	{
-		Log::Error("Usage: " + std::string(argv[0]) + " <config_file>");
+		Log::Error( "Usage: " + std::string( argv[0] ) + " <config_file>" );
 		return ;
 	}
-	myfile.open(argv[1], std::ios_base::in);
-	if (!myfile.is_open())
+	myfile.open( argv[1], std::ios_base::in );
+	if ( !myfile.is_open() )
 	{
-		Log::Error("Invalid file: " + std::string(argv[1]));
+		Log::Error( "Invalid file: " + std::string( argv[1] ) );
 		return ;
 	}
-	while (std::getline (myfile, line))
+	while ( std::getline ( myfile, line ) )
 		content += line + "\n";
 	myfile.close();
-	while (content.length() > 0)
+	while ( content.length() > 0 )
 	{
-		std::string head;
-		std::string body;
-		if (TreeSplit::get_pair(head, body, content))
+		if ( TreeSplit::get_pair( head, body, content ) )
 		{
 			try
 			{
-				ServerConfig sc(head, body);
-				serversConfig.push_back(sc);
+				ServerConfig	sc( head, body );
+				serversConfig.push_back( sc );
 			}
-			catch(const std::exception& e)
+			catch ( const std::exception& e )
 			{
+				Log::Error( e.what() );
 				//TODO LogError and clean
 				;
 			}
 		}
+		head.clear();
+		body.clear();
 	}
 }
 
-ConfigParser::~ConfigParser()
+ConfigParser::~ConfigParser( void )
 {
 }
 
-std::vector<ServerConfig>& ConfigParser::getServersConfig(void)
+std::vector<ServerConfig>& ConfigParser::getServersConfig( void )
 {
-	return (serversConfig);
+	return ( serversConfig );
 }
