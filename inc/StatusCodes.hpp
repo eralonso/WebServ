fc10203f10a632ce6685364dab93247452a43664
Eric Alonso Martinez <eralonso@car4s1.42barcelona.com>
2023-12-17 17:19:08 +0100

    WebServ < serverSockets > - [ Clean StatusCodes ]

--- inc/StatusCodes.hpp
+++ inc/StatusCodes.hpp
@@ -6,26 +6,33 @@
 /*   By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 18:41:19 by omoreno-          #+#    #+#             */
-/*   Updated: 2023/11/16 19:29:16 by omoreno-         ###   ########.fr       */
+/*   Updated: 2023/12/17 17:18:37 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _STATUSCODES_HPP_
 # define _STATUSCODES_HPP_
-# define CODES_NB 69
+
 # include <string>
+
+# define CODES_NB 69
 
 class StatusCodes
 {
 private:
-	StatusCodes();
-	StatusCodes(const StatusCodes&);
-	~StatusCodes();
-	StatusCodes operator+(const StatusCodes&);
+	StatusCodes( void );
+	StatusCodes( const StatusCodes& );
+	~StatusCodes( void );
+	StatusCodes	operator=( const StatusCodes& );
 public:
-	typedef struct s_dict {unsigned int code; const char *desc;}	t_dict;
-	static const t_dict dict[CODES_NB];
-	static const std::string decode(unsigned int code);
+	typedef struct s_dict {
+		unsigned int code;
+		const char *desc;
+	}	t_dict;
+public:
+	static const t_dict	dict[ CODES_NB ];
+public:
+	static const std::string	decode( unsigned int code );
 };
 
 #endif
